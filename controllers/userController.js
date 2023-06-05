a74daab45039d86bb0766c286819138dfcf9baaf
pexApred <120075705+pexApred@users.noreply.github.com>
2023-06-05 14:06:32 -0400

    Starting to work on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,9 @@
 const { objectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
+module.exports = {
+    // GET all users
+    async getAllUsers(req, res) {
+        try {
+            
+        }
